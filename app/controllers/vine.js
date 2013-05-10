2dd2ebe94b4eec35da59b43d92328f7fd13ae5f3
Selby Walker <selby.e.walker@gmail.com>
2013-05-10 16:02:13 -0700

    now storing requesting ip address in mongodb

--- app/controllers/vine.js
+++ app/controllers/vine.js
@@ -15,11 +15,12 @@ exports.share = function (req, res) {
 
 exports.get = function(req, res) {
   var query  = req.query.keywords,
+      location = req.connection.remoteAddress,
       search = 'vine ' + query;
       search = qs.stringify({ q: search });
 
-  request("http://search.twitter.com/search.json?" + search, function (e, r, body) {
-    if (e) { throw e }
+  request('http://search.twitter.com/search.json?' + search, function (e, r, body) {
+    if (e) { throw e; }
     var tweets = JSON.parse(body).results;
 
     if (Object.keys(tweets).length < 5) {
@@ -48,9 +49,8 @@ exports.get = function(req, res) {
             vineVids.push(vid);
             userNames[vid.username] = true;
           }
-
           if (vineVids.length === 8) {
-            var share = new Share({query: query, vid: vineVids});
+            var share = new Share({query: query, vid: vineVids, location: location});
             share.save(function (err, share) {
               if (err) { return console.log(err); }
               res.render('vine/show', { query: query, vids: vineVids, id: share.id });
